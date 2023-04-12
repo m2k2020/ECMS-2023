4b5096b87210a2167dbbb190acc8aafb24592ab2
musharaf Faqi <maamaanka55@gmail.com>
2023-04-12 14:38:37 +0300

    Success

--- ecmsapp/views.py
+++ ecmsapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from .models import House
 
 # Create your views here.
 def index(request):
@@ -47,16 +48,25 @@ def roles(request):
 #region House
 
 def house(request):
+
+    
+    houses = House.objects.filter(status=0) 
+    context = {'data': houses}
+
     if request.method == 'POST':
         new_district = request.POST['district']
         new_type = request.POST['type']
         new_houseno = request.POST['houseno']
         new_status = request.POST['status']
 
-        print(new_district, new_type, new_houseno, new_status)
+        if new_district != "" and new_type != "" and new_houseno != "" and new_status != "":
+            add_house = House(district=new_district, type=new_type, houseno=new_houseno, status=new_status)
+            add_house.save()
+        else:
+            print("bad")
 
         
-    return render(request,'Enviroment/house.html')
+    return render(request,'Enviroment/house.html',context)
 
 def createHouse(request):
     new_district = request.POST['district']
