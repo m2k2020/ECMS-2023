2b5a575717710dc168f14b5efc179e06feb5a114
musharaf Faqi <maamaanka55@gmail.com>
2023-04-12 20:28:42 +0300

    Success

--- ecmsapp/views.py
+++ ecmsapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models import House,Renter
+from .models import House,Renter,Enviroment
 from django.http import JsonResponse
 
 # Create your views here.
@@ -100,13 +100,25 @@ def renter(request):
     return render(request,'Enviroment/renter.html',context)
 
 
-def Enviroment(request):
+def enviroment(request):
     houses = House.objects.filter(status=0)
     renters = Renter.objects.filter(status=0)
+    # enivroments = Enviroment.objects.filter(status=0)
     context = {
         'houseData': houses,
-        'renterData': renters
+        'renterData': renters,
+        # 'enviromentData':enivroments
     }
+
+    if request.method == 'POST':
+        new_house = request.POST['houseno']
+        new_renter = request.POST['renter']
+        new_date = request.POST['regoster_date']
+        new_status = request.POST['status']
+
+        if new_house != "" and new_renter != "" and new_date != "" and new_status != "":
+            add_environment = Enviroment(house_id=new_house,renter_id=new_renter,regoster_date=new_date,status=new_status,)
+            add_environment.save()
     return render(request,'Enviroment/enviroment.html',context)
 
 
