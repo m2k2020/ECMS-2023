215efa0ba75abf73ead102e235d87410f014a84a
musharaf Faqi <maamaanka55@gmail.com>
2023-04-13 19:33:27 +0300

    Success

--- ecmsapp/urls.py
+++ ecmsapp/urls.py
@@ -4,16 +4,11 @@ from ecmsapp import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('forgot/', views.forgot, name='forgot'),
-    path('all_users/', views.all_users, name='all_users'),
-    path('admins/', views.admins, name='admins'),
     path('staffs/', views.staffs, name='staffs'),
-    path('clients/', views.clients, name='clients'),
-    path('permissions/', views.permissions, name='permissions'),
-    path('groups/', views.groups, name='groups'),
-    path('roles/', views.roles, name='roles'),
 
     path('fetch_data/',views.fetch_data, name='fetch_data'),
 
@@ -32,7 +27,7 @@ urlpatterns = [
 
 
     path('cleaning/', views.cleaning, name='cleaning'),
-    path('Enviroment_cleaned/', views.Enviroment_cleaned, name='Enviroment_cleaned'),
+    
     path('reports/', views.reports, name='reports'),
     path('Payment_Method/', views.Payment_Method, name='Payment_Method'),
     path('Transaction/', views.Transaction, name='Transaction'),
