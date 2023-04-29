b8d1e9de3c01becda21fc3e6028e006020dbafad
musharaf Faqi <maamaanka55@gmail.com>
2023-04-29 17:31:07 +0300

    Success

---
+++ ecmsapp/Code/__init__.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,redirect
+from models import House,Renter,Enviroment,Service,Users,Transaction
+from django.http import JsonResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'main.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def register(request):
+    return render(request,'accounts/register.html')
+
+def forgot(request):
+    return render(request,'accounts/forgot.html')
+
+
+def staffs(request):
+    return render(request,'accounts/staffs.html')
