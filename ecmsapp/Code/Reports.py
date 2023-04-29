30008a6de89a4d9b3053310c8c05a2aa14937fcd
musharaf Faqi <maamaanka55@gmail.com>
2023-04-29 17:41:27 +0300

    Success

---
+++ ecmsapp/Code/Reports.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,redirect
+from models import Service,Enviroment
+from django.http import JsonResponse
+
+# Create your views here.
+
+
+
+def reports(request):
+    return render(request,'Enviroment/reports.html')
+
+
+
+def Reports2(request):
+    return render(request,'Enviroment/Reports2.html')
