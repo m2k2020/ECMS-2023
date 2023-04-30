8ac8925c316b22213d6288f307e41f2e1d2f23ac
musharaf Faqi <maamaanka55@gmail.com>
2023-04-30 15:33:53 +0300

    Success

--- ecmsapp/Code/Renter.py
+++ ecmsapp/Code/Renter.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
 from ecmsapp.models import Renter
 from django.http import JsonResponse
 
@@ -20,3 +20,28 @@ def renter(request):
             add_renter = Renter(name=new_name, tell=new_tell, martial_status=new_martial_status, status=new_status)
             add_renter.save()
     return render(request,'Enviroment/renter.html',context)
+
+def update_renter(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        name = request.POST.get('name')
+        tell = request.POST.get('tell')
+        martial_status = request.POST.get('martial_status')
+
+        
+        renterUpdate = Renter.objects.get(id=id)
+        
+        # print(f"{id} {name} {tell} {martial_status}")
+        renterUpdate.name = name
+        renterUpdate.tell = tell
+        renterUpdate.martial_status = martial_status
+
+        renterUpdate.save()
+
+        isError = False
+        if isError:
+            message = "Failure updating"
+            return HttpResponse(message)
+        else:
+            message = "Successfully Update"
+            return HttpResponse(message)
