77e4530d40d7a52d41e1a0ee92b22de9dd900835
musharaf Faqi <maamaanka55@gmail.com>
2023-05-03 17:49:04 +0300

    Success

--- ecmsapp/Code/Transactions.py
+++ ecmsapp/Code/Transactions.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render,redirect
-from ecmsapp.models import Renter
+from ecmsapp.models import Service
 from django.http import JsonResponse
 
 # Create your views here.
 
 
 def Transaction(request):
-    return render(request,'Enviroment/Transaction.html')
+    serviceList = Service.objects.filter(status=0)
+    data = {
+        'dataService': serviceList
+    }
+    return render(request,'Enviroment/Transaction.html',data)
