0f4dc1ca8616fc39019020bdeab5df008083e8ed
gaay4229 <mosharafhilwa@gmail.com>
2023-05-12 11:15:43 +0300

    Success

--- ecmsapp/static/assets/scripts/enviroment/Transaction.js
+++ ecmsapp/static/assets/scripts/enviroment/Transaction.js
@@ -21,21 +21,53 @@ function viewTransaction(){
         $year = $(this).data('year');
 
         $ref = $houseno +$year +"100"+$id
+        $amount = "$"+$price+".00"
 
         $('#refno').text($ref)
 
         $('#name').text($renter)
-        $('#telephone').text($tel)
-        $('#martial').text($martial)
+        $('#Intelephone').text($tel)
+        $('#Inmartial').text($martial)
 
+
+
+        $('#Indistrict').text($district)
+        $('#Intype').text($type)
+        $('#Inhouseno').text($houseno)
+
+        $('#customerinfo').text($renter)
+        $('#datepay').text($date)
+        $('#amount').text($amount)
         
-
+        
+        
+        
     })
 }
 
 
 function printInvoice(){
     $('.printer').click(function (){
+        hidden()
         window.print()
+        show()
     })
+}
+function hidden(){
+    $('#hiddenNav').hide()
+    $('#hiddenSide').hide()
+    $('#hiddenSetting').hide()
+    $('#hiddenTable').hide()
+    $('#hiddenFooter').hide()
+    $('#hiddecardheader').hide()
+    $('.close').hide()
+}
+function show(){
+    $('#hiddenNav').show()
+    $('#hiddenSide').show()
+    $('#hiddenSetting').show()
+    $('#hiddenTable').show()
+    $('#hiddenFooter').show()
+    $('#hiddecardheader').show()
+    $('.close').show()
 }
