f5dfc73b0595e6b78d870d9ce2eb9220bd04e10d
musharaf Faqi <maamaanka55@gmail.com>
2023-05-15 16:52:21 +0300

    Success

--- ecmsapp/static/assets/scripts/enviroment/Transaction.js
+++ ecmsapp/static/assets/scripts/enviroment/Transaction.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
     viewTransaction()
-    printInvoice()
+    // printInvoice()
     
 })
 
 function viewTransaction(){
     $('.viewTransaction').click(function ()
     {
-        // e.preventDefault()
         $('#invoice').modal('show');
         $id = $(this).data('id');
         $renter = $(this).data('renter');
@@ -19,25 +18,64 @@ function viewTransaction(){
         $date = $(this).data('date');
         $price = $(this).data('price');
         $year = $(this).data('year');
-
         $ref = $houseno +$year +"100"+$id
         $amount = "$"+$price+".00"
 
-        $('#refno').text($ref)
+        // $('#refno').text($ref)
 
-        $('#name').text($renter)
-        $('#Intelephone').text($tel)
-        $('#Inmartial').text($martial)
+        $today = $('#today').text()
+
+        $.ajax({
+            url: '/generateInvoice/',
+            type: 'POST',
+            data : {
+                'name': $renter,
+                'refno':$ref,
+                'today':$today,
+                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+
+            },
+            success: function(response){
+                $('#refno').text($ref)
+
+                $('#name').text($renter)
+                $('#Intelephone').text($tel)
+                $('#Inmartial').text($martial)
 
 
 
-        $('#Indistrict').text($district)
-        $('#Intype').text($type)
-        $('#Inhouseno').text($houseno)
+                $('#Indistrict').text($district)
+                $('#Intype').text($type)
+                $('#Inhouseno').text($houseno)
 
-        $('#customerinfo').text($renter)
-        $('#datepay').text($date)
-        $('#amount').text($amount)
+                $('#customerinfo').text($renter)
+                $('#datepay').text($date)
+                $('#amount').text($amount)
+
+                $('.printer').click(function (){
+                    hidden()
+                    window.print()
+                    $.ajax({
+                        url: '/printInvoice/',
+                        type: 'POST',
+                        data:{
+                            'ref':$ref,
+                            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+                        }
+                        ,success: function (response){
+                            // console.log(response);
+                            show()
+                        }
+                    })
+                    
+                })
+                
+
+                // console.log(response)
+            }
+        })
+        
+        
         
         
         
