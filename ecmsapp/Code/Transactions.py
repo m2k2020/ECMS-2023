40cdfd34ee8ed0763b4a8aa6eb72abea695417e2
musharaf Faqi <maamaanka55@gmail.com>
2023-05-15 16:52:14 +0300

    Success

--- ecmsapp/Code/Transactions.py
+++ ecmsapp/Code/Transactions.py
@@ -1,11 +1,17 @@
-import datetime
+import datetime,platform,socket
+from user_agents import parse
 from django.shortcuts import render,redirect
-from ecmsapp.models import Service,Transaction
+from ecmsapp.models import Service,Transaction,userLoggers
 from datetime import date
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
+
+p = platform.uname()
+host =socket.gethostname()
+ipaddress = socket.gethostbyname(host)
+
 
 @login_required(login_url='user_login')
 def transaction(request):
@@ -27,6 +33,9 @@ def transaction(request):
 
 @login_required(login_url='user_login')
 def makePayment(request):
+    user_agent_string = request.META.get('HTTP_USER_AGENT')
+    user_agent = parse(user_agent_string)
+    userinfo = f'{ipaddress} / {user_agent}'
 
     if request.method == 'POST':
         pay_service = request.POST.get('service')
@@ -35,16 +44,16 @@ def makePayment(request):
         pay_price = 5
         pay_status = 0
 
-        msg = ""
+        info = ""
 
         servid = Service.objects.get(id=pay_service)
 
-        msg += f"Renter : {servid.enviroment.renter.name}-{servid.enviroment.renter.tell}\n"
-        msg += f"House : {servid.enviroment.house.district}-{servid.enviroment.house.houseno}-({servid.enviroment.house.type})\n"
-        msg += f"Account : {accounts}\n"
-        msg += f"date : {pay_date} and Price {pay_price}\n"
+        info += f"Renter : {servid.enviroment.renter.name}-{servid.enviroment.renter.tell}\n"
+        info += f"House : {servid.enviroment.house.district}-{servid.enviroment.house.houseno}-({servid.enviroment.house.type})\n"
+        info += f"Account : {accounts}\n"
+        info += f"date : {pay_date} and Price {pay_price}\n"
 
-        print(msg)
+        # print(info)
         addrow = Transaction(service=servid,date=pay_date,status=pay_status,account=accounts,price=pay_price)
         addrow.save()
         if addrow:
@@ -52,13 +61,79 @@ def makePayment(request):
             servid.save()
             response = {
                 'success': True,
-                'message': f'Your transaction has been created by {msg}',
-                'error': f'Not Saved by {msg}'
+                'message': f'Your transaction has been created by {info}',
+                'error': f'Not Saved by {info}'
             }
+            
+            
+            msg = f"{servid.enviroment.renter.name} Was Been Successfuly Maked Payment with {servid.enviroment.house.district}-{servid.enviroment.house.houseno}-({servid.enviroment.house.type}) and Added to the System"
+            userLoggers(logger_name=request.user,device=userinfo,message=msg,level="INFO").save()
+            
+            
             return JsonResponse(response)
+            
         else:
             response = {
                 'error': True,
-                'message': f'Not Saved by {msg}'
+                'message': f'Not Saved by {info}'
             }
+            msg = f"{servid.enviroment.renter.name} Was Not Successfuly Maked Payment with {servid.enviroment.house.district}-{servid.enviroment.house.houseno}-({servid.enviroment.house.type}) and Added to the System"
+            userLoggers(logger_name=request.user,device=userinfo,message=msg,level="INFO").save()
             return JsonResponse(response)
+
+
+def generateInvoice(request):
+    
+    user_agent_string = request.META.get('HTTP_USER_AGENT')
+    user_agent = parse(user_agent_string)
+    userinfo = f'{ipaddress} / {user_agent}'
+
+    if request.method == "POST":
+        renter = request.POST.get("name")
+        ref = request.POST.get("refno")
+        today = request.POST.get("today")
+
+        msg = f"Reference No {ref} Has Been Genereated By {renter} as invoice and Added to the System"
+        userLoggers(logger_name=request.user,device=userinfo,message=msg,level="INFO").save()
+
+
+
+
+
+        print(f"{renter} {ref} {today}")
+
+        response = {
+            'success': True,
+            'message': 'Successfully generated'
+
+        }
+        return JsonResponse(response)
+    else:
+        redirect(transaction)
+
+def printInvoice(request):
+    
+    user_agent_string = request.META.get('HTTP_USER_AGENT')
+    user_agent = parse(user_agent_string)
+    userinfo = f'{ipaddress} / {user_agent}'
+
+    if request.method == "POST":
+        ref = request.POST.get("ref")
+
+        msg = f"Reference No {ref} Has Been Printed"
+        userLoggers(logger_name=request.user,device=userinfo,message=msg,level="INFO").save()
+
+
+        print(f" {ref} ")
+
+        response = {
+            'success': True,
+            'message': 'Successfully generated'
+
+        }
+        return JsonResponse(response)
+    else:
+        redirect(transaction)
+
+
+
