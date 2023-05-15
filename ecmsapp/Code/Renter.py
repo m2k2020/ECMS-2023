dab3b9966ec92646ba68900d24797a3402b491d5
musharaf Faqi <maamaanka55@gmail.com>
2023-05-15 15:19:29 +0300

    Success

--- ecmsapp/Code/Renter.py
+++ ecmsapp/Code/Renter.py
@@ -1,9 +1,15 @@
+import platform,socket
+from user_agents import parse
 from django.shortcuts import render,redirect,HttpResponse
-from ecmsapp.models import Renter
+from ecmsapp.models import Renter,userLoggers
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
+
+p = platform.uname()
+host =socket.gethostname()
+ipaddress = socket.gethostbyname(host)
 
 @login_required(login_url='user_login')
 def renter(request):
@@ -15,6 +21,9 @@ def renter(request):
 
 @login_required(login_url='user_login')
 def createRenter(request):
+    user_agent_string = request.META.get('HTTP_USER_AGENT')
+    user_agent = parse(user_agent_string)
+    userinfo = f'{ipaddress} / {user_agent}'
     if request.method == 'POST':
         new_name = request.POST['name']
         new_tell = request.POST['tell']
@@ -24,13 +33,23 @@ def createRenter(request):
         if new_name != "" and new_tell != "" and new_martial_status != "" and new_status != "":
             add_renter = Renter(name=new_name, tell=new_tell, martial_status=new_martial_status, status=new_status)
             add_renter.save()
+            msg = f"({new_name}-{new_tell}-{new_martial_status}) Renter Has Successfuly Added to the System"
+            userLoggers(logger_name=request.user,device=userinfo,message=msg,level="INFO").save()
             isError = True
             return HttpResponse(isError)
         else:
+            
+            msg = f"({new_name}-{new_tell}-{new_martial_status}) Renter Not Successfuly Added to the System"
+            userLoggers(logger_name=request.user,device=userinfo,message=msg,level="INFO").save()
             isError = False
             return HttpResponse(isError)
+        
+
 @login_required(login_url='user_login')
 def update_renter(request):
+    user_agent_string = request.META.get('HTTP_USER_AGENT')
+    user_agent = parse(user_agent_string)
+    userinfo = f'{ipaddress} / {user_agent}'
     if request.method == 'POST':
         id = request.POST.get('id')
         name = request.POST.get('name')
@@ -47,6 +66,16 @@ def update_renter(request):
 
         renterUpdate.save()
 
+        if renterUpdate:
+            
+            msg = f"({renterUpdate.name}-{renterUpdate.tell}-{renterUpdate.martial_status}) Renter Has Successfuly Updated to the System"
+            userLoggers(logger_name=request.user,device=userinfo,message=msg,level="INFO").save()
+
+        else:
+            msg = f"({renterUpdate.name}-{renterUpdate.tell}-{renterUpdate.martial_status}) Renter Not Successfuly Updated to the System"
+            userLoggers(logger_name=request.user,device=userinfo,message=msg,level="INFO").save()
+            
+
         isError = False
         if isError:
             message = "Failure updating"
@@ -59,6 +88,9 @@ def update_renter(request):
 
 @login_required(login_url='user_login')
 def delete_renter(request):
+    user_agent_string = request.META.get('HTTP_USER_AGENT')
+    user_agent = parse(user_agent_string)
+    userinfo = f'{ipaddress} / {user_agent}'
     if request.method == 'POST':
         id = request.POST.get('id')
         status = request.POST.get('status')
@@ -71,6 +103,15 @@ def delete_renter(request):
         renterUpdate.status = status
 
         renterUpdate.save()
+
+        if renterUpdate:
+            
+            msg = f"({renterUpdate.name}-{renterUpdate.tell}-{renterUpdate.martial_status}) Renter Has Successfuly Deleted to the System"
+            userLoggers(logger_name=request.user,device=userinfo,message=msg,level="INFO").save()
+        else:
+            msg = f"({renterUpdate.name}-{renterUpdate.tell}-{renterUpdate.martial_status}) Renter Not Successfuly Deleted to the System"
+            userLoggers(logger_name=request.user,device=userinfo,message=msg,level="INFO").save()
+
 
         isError = False
         if isError:
